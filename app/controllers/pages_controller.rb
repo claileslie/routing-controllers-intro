0c106ced254b7e1c44671fb79caaaab0121db376
CLAIRE LESLIE <c.leslie@live.ca>
2017-01-30 13:54:46 -0500

    create flash notice to contest method and redirect to '/welcome'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
   end
 
   def contest
-    @header = "Gagner!"
+    flash[:notice] = "Sorry, the contest has ended"
     redirect_to "/welcome"
   end
 
