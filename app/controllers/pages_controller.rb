223b34ba756f4b7890f43db854b21d3433fd09b1
CLAIRE LESLIE <c.leslie@live.ca>
2017-01-30 13:33:11 -0500

    create @header instance variable and provide different values for it in the different instance methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,16 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "Voici les chats"
 
   end
 
   def about
-
+    @header = "Nous sommes les chats"
   end
 
   def contest
-
+    @header = "Gagner!"
   end
 
 end
